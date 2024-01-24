8cf905d8c925407ed39af06578f065ffec6d4090
you_tu <you_tu@naver.com>
2024-01-24 16:46:41 +0900

    - log4j -> log4j2 변경 - db connection 작업중

--- src/main/java/test_project/com/ctl/MainController.java
+++ src/main/java/test_project/com/ctl/MainController.java
@@ -2,18 +2,19 @@ package test_project.com.ctl;
 
 import java.util.Locale;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MainController {
-	private static final Logger logger = LoggerFactory.getLogger(MainController.class);
+	private static final Logger logger = LogManager.getLogger(MainController.class);
 	
 	@RequestMapping(value = "/map3d")
 	public String home(Locale locale, Model model) {
+		logger.info("Welcome 3DMap!! {}", locale);
 		return "/main/map";
 	}
 }
