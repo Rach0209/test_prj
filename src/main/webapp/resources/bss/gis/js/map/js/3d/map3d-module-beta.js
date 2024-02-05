5a5026c245a868d35b362a16e91e87a1ffcc6bc2
you_tu <you_tu@naver.com>
2024-02-05 11:37:01 +0900

    -

--- src/main/webapp/resources/bss/gis/js/map/js/3d/map3d-module-beta.js
+++ src/main/webapp/resources/bss/gis/js/map/js/3d/map3d-module-beta.js
@@ -10,39 +10,39 @@ var Module = {
 
 function init() {
 	const vworld = "https://xdworld.vworld.kr";
-		
-		// 엔진 초기화 API 호출(필수)
-		Module.initialize({
-			container: document.getElementById("map3D"),
-			terrain : {
-				dem : {
-					url : vworld,
-					name : "dem",
-					servername : "XDServer3d",
-					encoding : true
-				},
-				image : {
-					url : vworld,
-					name : "tile_mo_HD",
-					servername : "XDServer3d"
-				},
+	
+	// 엔진 초기화 API 호출(필수)
+	Module.initialize({
+		container: document.getElementById("map3D"),
+		terrain : {
+			dem : {
+				url : vworld,
+				name : "dem",
+				servername : "XDServer3d",
+				encoding : true
 			},
-			worker : {
-				use : true,
-				path : "/resources/bss/gis/js/map/engine_beta/worker/XDWorldWorker.js",
-				count : 5
+			image : {
+				url : vworld,
+				name : "tile_mo_HD",
+				servername : "XDServer3d"
 			},
-			defaultKey : "ezbBD(h2eFCmDQFQd9QpdzDS#zJRdJDm4!Epe(a2EzcbEzb2"
-		});
+		},
+		worker : {
+			use : true,
+			path : "/resources/bss/gis/js/map/engine_beta/worker/XDWorldWorker.js",
+			count : 5
+		},
+		defaultKey : "ezbBD(h2eFCmDQFQd9QpdzDS#zJRdJDm4!Epe(a2EzcbEzb2"
+	});
 		
-		var layer = Module.getTileLayerList().createXDServerLayer({
-			url : "https://xdworld.vworld.kr",
-			servername : "XDServer3d",
-			name : "facility_build",
-			type : 9,
-			minLevel : 0,
-			maxLevel : 15
-		});
+	var layer = Module.getTileLayerList().createXDServerLayer({
+		url : "https://xdworld.vworld.kr",
+		servername : "XDServer3d",
+		name : "facility_build",
+		type : 9,
+		minLevel : 0,
+		maxLevel : 15
+	});
 }
 
 var script = document.createElement('script');
